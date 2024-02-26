0ca0b7a99f50e69087adcc0e1b84346ad82921bd
Nisa Yavuz <nsnryvz@outlook.com>
2024-02-26 23:26:14 +0300

    Generics & Linq

---
+++ Generics/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            sehirler.Add("İstanbul");
+            sehirler.Add("İstanbul");
+            sehirler.Add("İstanbul");
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("Trabzon");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("İstanbul");
+
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        private T[] _tempArray;
+
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // IPersonManager personManager = new IPersonManager(); ---> interfaceler newlenemez.
+
+            //IPersonManager customerManager = new CustomerManager();
+
+            //IPersonManager employeeManager = new EmployeeManager();   
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi.");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi.");
+        }
+    }
+
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
         }
     }
 }
